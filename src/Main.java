25eb6a692f72e4e02754882abb44f382d85977d6
Yong Bakos <ybakos@humanoriented.com>
2014-11-16 13:19:34 -0600

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import processing.core.*;
+
+public class Main {
+
+    private final static String[] OPTIONS = new String[] { "Sketch", "--present"};
+    
+    public static void main(String[] args) {
+        System.out.println("A sample Processing program in pure Java.");
+        PApplet.main(OPTIONS);
+    }
+
+}
---
+++ src/Sketch.java
@@ -0,0 +1,13 @@
+import processing.core.*;
+
+public class Sketch extends PApplet {
+    
+    public void setup() {
+        size(displayWidth, displayHeight);
+    }
+
+    public void draw() {
+        background(0);
+    }
+
+}
