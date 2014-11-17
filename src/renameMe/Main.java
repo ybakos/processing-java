a4966efee6704d4ecf9ae723ae3a56cc8ffacf65
Yong Bakos <ybakos@humanoriented.com>
2014-11-16 21:03:26 -0600

    Add complete build file. Fix order of PApplet args for present mode.

--- src/renameMe/Main.java
+++ src/renameMe/Main.java
@@ -4,7 +4,7 @@ import processing.core.*;
 
 public class Main {
 
-    private final static String[] OPTIONS = new String[] { "Sketch", "--present"};
+    private final static String[] OPTIONS = new String[] { "--present", "renameMe.Sketch" };
     
     public static void main(String[] args) {
         System.out.println("A sample Processing program in pure Java.");
